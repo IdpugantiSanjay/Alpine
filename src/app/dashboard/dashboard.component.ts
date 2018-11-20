574a705a55270a95c066922796355971fc0feebd
Sanjay <sanjay11@outlook.com>
2018-11-20 22:27:02 +0530

    Added routing after creating new task

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -4,6 +4,7 @@ import { TaskService } from '../services/task.service';
 import { Task } from '../interfaces/task';
 import { FormGroup, FormControl } from '@angular/forms';
 import { debounceTime, switchMap, share, shareReplay, tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 
 @Component({
@@ -18,7 +19,7 @@ export class DashboardComponent implements OnInit {
     searchField: new FormControl('')
   });
 
-  tasks;
+  tasks: Observable<Task[]>;
 
   constructor(private taskService: TaskService) { }
 
@@ -27,17 +28,24 @@ export class DashboardComponent implements OnInit {
 
     this.searchTasksForm.controls['searchField'].valueChanges
       .pipe(
+        // wait for 1 second after user types
         debounceTime(1000),
         share(),
+        // return list of tasks which contains search strings
         switchMap(value => this.tasks = this.taskService.tasks(value)),
       )
       .subscribe();
   }
 
+  /**
+   * Delete task and display list
+   * @param taskId id of the task to delete
+   */
   onTaskDeleteEvent(taskId: string) {
     this.taskService.deleteTask(taskId)
       .pipe(
-        switchMap(() => this.tasks = this.taskService.tasks())
+        // after deleting the task call task list again to display latest data
+        switchMap(() => this.tasks = this.taskService.tasks())  
       )
       .subscribe();
   }
